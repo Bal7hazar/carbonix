cd78c45d86787cf607b93cdf387ecaebb851309a
Bal7hazar <bal7hazar@proton.me>
2022-06-02 17:05:20 +0200

    :wrench: minor modification

--- carbonix/app.py
+++ carbonix/app.py
@@ -2,8 +2,6 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 """App main module."""
 
-import os
-
 from carbonix.controllers.dashboard_controller import DashboardController
 
 if __name__ == "__main__":
