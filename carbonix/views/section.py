eb46cc974f509a80f174757c7d86d7df4a7b4170
Bal7hazar <bal7hazar@proton.me>
2022-06-12 17:13:48 +0200

    :wrench: clean and improve css

--- carbonix/views/section.py
+++ carbonix/views/section.py
@@ -1,18 +1,8 @@
 """Section view module."""
 
-from dash import html
-
-from carbonix.views import GREEN, PURPLE
-
 
 class Section:
     """Section class."""
 
-    @staticmethod
-    def linear_gradian_spans(title):
-        """Return colored spans."""
-        colors = [color.hex_l for color in PURPLE.range_to(GREEN, len(title))]
-        return [
-            html.Span(letter, style={"color": f"{color}"})
-            for letter, color in zip(title, colors)
-        ]
+    def section(self):
+        raise NotImplementedError
